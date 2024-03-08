9afcb42e082a98fa3328391414801df63d7d09f2
rosny <rosny@smylee.com>
2024-03-08 19:27:32 +0300

    add function to remove repeated an odd number of times

--- oddOnesOut.ts
+++ oddOnesOut.ts
@@ -1,4 +1,4 @@
-/*  ______ Instruction ____ 
+/*  ______ Instructions ____ 
 Given a list of numbers. The numbers each repeat a certain number of times. 
 source: https://www.codewars.com/kata/5d376cdc9bcee7001fcb84c0/train/typescript */
 
